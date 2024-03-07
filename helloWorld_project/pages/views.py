e25d01856fd27dbd55c1ad3da4d1ca9b4eb26733
Daniel Parra Palomo <axlfire@axlfire-VivoBook-ASUSLaptop-X430FA-S430FA>
2024-03-07 17:46:48 -0500

    temino puntos antes del C

--- helloWorld_project/pages/views.py
+++ helloWorld_project/pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request): 
-    return HttpResponse('Hello World!') 
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+    
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+    
